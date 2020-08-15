27f4b53f411cf9c9325f91e3f4667c80875cb3ef
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2020-08-14 23:02:28 -0300

    Implementing Mkdir and Ls Commands

--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -3,6 +3,35 @@ package com.rtjvm.scala.oop.files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry] )
   extends DirEntry(parentPath, name) {
 
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory =
+    new Directory(parentPath, name, contents.filter(e => !e.name.equals(entryName)) :+ newEntry)
+
+  def findEntry(entryName: String): DirEntry = {
+    def findEntryHelper(name: String, contentList: List[DirEntry]): DirEntry = {
+      if (contentList.isEmpty) null
+      else if (contentList.head.name.equals(name)) contentList.head
+      else findEntryHelper(name, contentList.tail)
+    }
+    findEntryHelper(entryName, contents)
+  }
+
+  def addEntry(newEntry: DirEntry): Directory =
+    new Directory(parentPath, name, contents :+ newEntry)
+
+  def findDescendant(path: List[String]): Directory =
+    if (path.isEmpty) this
+    else findEntry(path.head).asDirectory.findDescendant(path.tail)
+
+  def getAllFoldersInPath(): List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList.filter(x => !x.isEmpty)
+
+  def hasEntry(name: String): Boolean =
+    findEntry(name) != null
+
+  override def asDirectory: Directory = this
+
+  override def getType: String =
+    "Directory"
 }
 
 object Directory {
