fafd42e34097a428a2f910369a3b2f512bc1347b
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2020-08-16 23:00:51 -0300

    Some refactorings

--- src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
+++ src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -1,7 +1,5 @@
 package com.rtjvm.scala.oop.filesystem
 
-import java.util.Scanner
-
 import com.rtjvm.scala.oop.commands.Command
 import com.rtjvm.scala.oop.files.Directory
 
@@ -12,13 +10,20 @@ import com.rtjvm.scala.oop.files.Directory
 object Filesystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root = root, wd = root)
-  val scanner = new Scanner(System.in)
 
-  while(true) {
-    state.show
-    val input = scanner.nextLine()
-    state = Command.from(input).apply(state)
-  }
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show
+    Command.from(newLine).apply(currentState)
+  })
+
+//
+//  var state = State(root = root, wd = root)
+//  val scanner = new Scanner(System.in)
+//
+//  while(true) {
+//    state.show
+//    val input = scanner.nextLine()
+//    state = Command.from(input).apply(state)
+//  }
 
 }
