06329fba62443cfe741f10b4e10b2122fd0b64ca
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2020-08-16 22:12:22 -0300

    Implementing Echo Command

---
+++ src/com/rtjvm/scala/oop/commands/Echo.scala
@@ -0,0 +1,50 @@
+package com.rtjvm.scala.oop.commands
+import com.rtjvm.scala.oop.filesystem.State
+
+import scala.annotation.tailrec
+
+class Echo(args: Array[String]) extends Command {
+
+  override def apply(state: State): State = {
+    /*
+        if no args, state
+        else if just on arg, print to console
+        else if  multiple args
+        {
+          operator = next to last argument
+          if >
+            echo to a file
+          if >>
+            append to a file
+          else
+            just echo everything to console
+        }
+     */
+
+
+    if (args.isEmpty) state
+    else if (args.length == 1) state.setMessage(args(0))
+    else {
+      val operator = args(args.length - 2)
+      val filename = args(args.length - 1)
+      val contents = createContent(args, args.length - 2)
+
+      if (">>".equals(operator))
+        doEcho(state, contents, filename, append = true)
+      else if (">".equals(operator))
+        doEcho(state, contents, filename, append = false)
+      else state.setMessage(createContent(args, args.length))
+    }
+  }
+
+  def doEcho(state: State, content: String, filename: String, append: Boolean): State = ???
+
+  def createContent(args: Array[String], topIndex: Int): String = {
+    @tailrec
+    def createContentHelper(currentIndex: Int, accumulator: String): String =
+      if (currentIndex >= topIndex) accumulator
+      else createContentHelper(currentIndex + 1, accumulator + " " + args(currentIndex))
+
+    createContentHelper(0, "")
+  }
+}
