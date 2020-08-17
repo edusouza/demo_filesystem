b3932814a74b427816a4fc2e7ebfe2293d21a6b3
Eduardo Olievira de Souza <souza.eduardo@gmail.com>
2020-08-16 22:36:23 -0300

    Implementing Echo Command to append and replace contents

--- src/com/rtjvm/scala/oop/files/File.scala
+++ src/com/rtjvm/scala/oop/files/File.scala
@@ -2,6 +2,12 @@ package com.rtjvm.scala.oop.files
 
 class File(override val parentPath: String, override val name: String, contents: String)
   extends DirEntry(parentPath, name) {
+  def setContents(newContents: String): File =
+    new File(parentPath, name, newContents)
+
+  def appendContents(newContents: String): File =
+    setContents(contents + "\n" + newContents)
+
 
   override def asDirectory: Directory =
     throw new FilesystemException("File cannot be converted to a directory")
